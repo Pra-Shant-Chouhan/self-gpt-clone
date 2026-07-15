f1061ee2986eb41c70cc9363ebed27682003b50f
Prashant Chouhan <pchouhan122@gmail.com>
2026-07-15 18:41:21 +0530

    chapter-3 => Clerk setup and protect route with proxy only.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex py-4 bg-gray-200 flex-row items-center justify-between p-24">
       <h1>Hello, Next.js!</h1>
+      <UserButton />
       <ModeToggle />
-    </>
+    </div>
   );
 }
